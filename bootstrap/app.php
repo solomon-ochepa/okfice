7200130fd05d72269b90f8a1ae6cbc5b19c80895
Solomon Ochepa <solomonochepa@gmail.com>
2024-11-27 10:41:21 +0100

    Install necessary packages and update system configs

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->trustProxies(at: '*');
+        $middleware->alias([
+            // 'feature' => EnsureFeaturesAreActive::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
