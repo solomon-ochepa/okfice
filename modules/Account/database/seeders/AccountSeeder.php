02887e3b29a416f9e66e25d4d4f4c9b96759b53b
Solomon Ochepa <solomonochepa@gmail.com>
2024-12-16 05:24:11 +0100

    Update the account migration and seeder

--- modules/Account/database/seeders/AccountSeeder.php
+++ modules/Account/database/seeders/AccountSeeder.php
@@ -3,6 +3,7 @@
 namespace Modules\Account\Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Modules\Account\App\Models\Account;
 use Modules\User\App\Models\User;
 
 class AccountSeeder extends Seeder
@@ -13,9 +14,11 @@ class AccountSeeder extends Seeder
     public function run(): void
     {
         $users = User::doesntHave('accounts')->get();
+        $last_account_number = (int) Account::max('number');
         foreach ($users as $user) {
             $user->accounts()->create([
-                'primary' => true
+                'primary' => true,
+                'number' => ++$last_account_number,
             ]);
         }
     }
