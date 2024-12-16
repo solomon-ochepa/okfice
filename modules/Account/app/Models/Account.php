06be74e2f8b2bc1b0ba27da8a9afe05337e1a48e
Solomon Ochepa <solomonochepa@gmail.com>
2024-12-16 03:14:16 +0100

    Create the Account model and relationships - Create the user relationship - Create the transaction relationship

---
+++ modules/Account/app/Models/Account.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Modules\Account\App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Modules\Account\Database\Factories\AccountFactory;
+use Modules\Transaction\App\Models\Transaction;
+use Modules\User\App\Models\User;
+
+class Account extends Model
+{
+    use HasFactory, HasUuids, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = [
+        'user_id',
+        'balance',
+        'currency',
+    ];
+
+    protected static function newFactory(): Factory
+    {
+        return AccountFactory::new();
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
+}
