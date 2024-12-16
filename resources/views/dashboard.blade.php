f3c85d50784ad67921c51cebbf0961b49d35eb77
Solomon Ochepa <solomonochepa@gmail.com>
2024-12-16 02:33:53 +0100

    Create the Stats widget, and updated the Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,42 +1,12 @@
 <x-app-layout>
     <div id="appCapsule">
-
         <!-- Wallet Card -->
         <livewire:wallet-card />
 
-        <!-- Stats -->
-        <div class="section">
-            <div class="row mt-2">
-                <div class="col-6">
-                    <div class="stat-box">
-                        <div class="title">Income</div>
-                        <div class="value text-success">$ 552.95</div>
-                    </div>
-                </div>
-                <div class="col-6">
-                    <div class="stat-box">
-                        <div class="title">Expenses</div>
-                        <div class="value text-danger">$ 86.45</div>
-                    </div>
-                </div>
-            </div>
-            <div class="row mt-2">
-                <div class="col-6">
-                    <div class="stat-box">
-                        <div class="title">Total Bills</div>
-                        <div class="value">$ 53.25</div>
-                    </div>
-                </div>
-                <div class="col-6">
-                    <div class="stat-box">
-                        <div class="title">Savings</div>
-                        <div class="value">$ 120.99</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
         <!-- Transactions -->
         <livewire:transaction::recent />
+
+        <!-- Stats -->
+        <livewire:stats />
     </div>
 </x-app-layout>
