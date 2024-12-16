79191bb49fb5f8e1f68aef35f814f2de7000e095
Solomon Ochepa <solomonochepa@gmail.com>
2024-12-16 03:13:05 +0100

    Create the Account migration

---
+++ modules/Account/database/migrations/2024_12_16_020121_create_accounts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('user_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->decimal('balance')->default(0);
+            $table->string('currency')->default('N');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
