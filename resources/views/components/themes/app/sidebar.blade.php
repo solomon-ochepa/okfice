348bb818fd007c23d76ec09b14b7f6b3a1c3316d
Solomon Ochepa <solomonochepa@gmail.com>
2024-12-31 12:43:30 +0100

    `Update sidebar link to use x-a component`

--- resources/views/components/themes/app/sidebar.blade.php
+++ resources/views/components/themes/app/sidebar.blade.php
@@ -67,14 +67,14 @@
                 </div>
                 <ul class="listview flush transparent no-line image-listview">
                     <li>
-                        <a class="item" href="#">
+                        <x-a route="settings">
                             <div class="icon-box bg-primary">
                                 <ion-icon name="settings-outline"></ion-icon>
                             </div>
                             <div class="in">
                                 Settings
                             </div>
-                        </a>
+                        </x-a>
                     </li>
 
                     <li>
