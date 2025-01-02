333f7e8612b3100a205ba1e1e367c211e26815f7
Solomon Ochepa <solomonochepa@gmail.com>
2025-01-03 00:37:56 +0100

    Create TransactionEntry model and other resources

---
+++ modules/Transaction/app/Models/TransactionEntry.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Modules\Transaction\App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Modules\Account\App\Models\Account;
+use Modules\Transaction\Database\Factories\TransactionEntryFactory;
+
+class TransactionEntry extends Model
+{
+    use HasFactory, HasUuids;
+
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = [
+        'transaction_id',
+        'account_id',
+        'entry_type',
+        'amount',
+    ];
+
+    protected static function newFactory(): TransactionEntryFactory
+    {
+        return TransactionEntryFactory::new();
+    }
+
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class);
+    }
+
+    public function account()
+    {
+        return $this->belongsTo(Account::class);
+    }
+}
---
+++ modules/Transaction/database/seeders/TransactionEntrySeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Transaction\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class TransactionEntrySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
