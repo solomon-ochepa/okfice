b897d2337ad8792cb2f20c4ab601c947bd559085
Solomon Ochepa <solomonochepa@gmail.com>
2025-01-03 07:48:02 +0100

    Create Payment model and migration

---
+++ modules/Payment/app/Models/Payment.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Modules\Payment\App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Modules\Payment\Database\Factories\PaymentFactory;
+use Modules\User\App\Models\User;
+
+class Payment extends Model
+{
+    use HasFactory, HasUuids;
+
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = [
+        'user_id',
+        'method_id',
+        'details',
+        'default',
+    ];
+
+    protected static function newFactory(): PaymentFactory
+    {
+        return PaymentFactory::new();
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function method()
+    {
+        return $this->belongsTo(PaymentMethod::class);
+    }
+}
---
+++ modules/Payment/database/migrations/2025_01_03_062257_create_payments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('user_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignUuid('method_id')->constrained('payment_methods', 'slug')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->json('details');
+            $table->boolean('default')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
