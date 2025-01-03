ad988725e35431fbd7e59fb0821606b64c286ead
Solomon Ochepa <solomonochepa@gmail.com>
2025-01-03 06:56:41 +0100

    Update transaction model, create the entries relationship. Update migration

--- modules/Transaction/app/Models/Transaction.php
+++ modules/Transaction/app/Models/Transaction.php
@@ -5,8 +5,7 @@ namespace Modules\Transaction\App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
-// use Modules\Transaction\Database\Factories\TransactionFactory;
+use Modules\Transaction\Database\Factories\TransactionFactory;
 
 class Transaction extends Model
 {
@@ -15,10 +14,22 @@ class Transaction extends Model
     /**
      * The attributes that are mass assignable.
      */
-    protected $fillable = [];
+    protected $fillable = [
+        'type_id',
+        'session_id',
+        'description',
+        'status',
+        'created_by',
+        'date',
+    ];
 
-    // protected static function newFactory(): TransactionFactory
-    // {
-    //     // return TransactionFactory::new();
-    // }
+    protected static function newFactory(): TransactionFactory
+    {
+        return TransactionFactory::new();
+    }
+
+    public function entries()
+    {
+        return $this->hasMany(TransactionEntry::class);
+    }
 }
