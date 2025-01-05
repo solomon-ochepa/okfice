1afd07e79517dce1cff78922c1da0277d24c7266
Solomon Ochepa <solomonochepa@gmail.com>
2025-01-05 02:16:31 +0100

    Update payment routes and create a sidebar link

--- modules/Payment/routes/api.php
+++ modules/Payment/routes/api.php
@@ -5,6 +5,6 @@ use Modules\Payment\App\Http\Controllers\PaymentController;
 use Modules\Payment\App\Http\Controllers\PaymentMethodController;
 
 Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('payments', PaymentController::class)->names('payments');
-    Route::apiResource('payment-methods', PaymentMethodController::class)->names('payment-methods');
+    // Route::apiResource('payment-methods', PaymentMethodController::class)->names('payment-methods');
+    // Route::apiResource('payments', PaymentController::class)->names('payments');
 });
--- modules/Payment/routes/web.php
+++ modules/Payment/routes/web.php
@@ -3,8 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Payment\App\Http\Controllers\PaymentController;
 use Modules\Payment\App\Http\Controllers\PaymentMethodController;
+use Modules\Payment\app\Livewire\Index;
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('payments', PaymentController::class)->names('payments');
-    Route::resource('payment-methods', PaymentMethodController::class)->names('payment-methods');
+    // Route::resource('payment-methods', PaymentMethodController::class)->names('payment-methods');
+
+    Route::resource('payments', PaymentController::class)->except(['index'])->names('payments');
+    Route::get('payments', Index::class)->name('payments.index');
 });
