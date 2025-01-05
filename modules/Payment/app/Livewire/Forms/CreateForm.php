ec90b79afb9edbbd46d429bc4e976788a1c55d5d
Solomon Ochepa <solomonochepa@gmail.com>
2025-01-05 03:45:51 +0100

    Refactor payment creation workflow

--- modules/Payment/app/Livewire/Forms/CreateForm.php
+++ modules/Payment/app/Livewire/Forms/CreateForm.php
@@ -52,14 +52,23 @@ class CreateForm extends Form
             ]);
 
             $data = $this->all();
-
             $query = [
                 'user_id' => $user->id,
                 'method_id' => $payment_method->slug,
                 'details' => $data,
             ];
 
-            $payment = Payment::firstOrCreate($query);
+            $payment = Payment::where([
+                'details->bank->code' => $this->bank['code'],
+                'details->account->number' => $this->account['number'],
+                'method_id' => $payment_method->slug,
+            ]);
+
+            if ($payment->exists()) {
+                $payment = $payment->first();
+            } else {
+                $payment = Payment::firstOrCreate($query);
+            }
 
             if ($user->payments->count() == 1) {
                 $payment->default = true;
