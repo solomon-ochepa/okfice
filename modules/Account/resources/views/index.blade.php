4b8e2288eb07d6d591e555f98255a9418664d78d
Solomon Ochepa <solomonochepa@gmail.com>
2025-04-08 04:33:16 +0100

    Remove unused files

--- modules/Account/resources/views/index.blade.php
+++
@@ -1,5 +0,0 @@
-<x-app-layout>
-    <h1>Hello World</h1>
-
-    <p>Module: {!! config('account.name') !!}</p>
-</x-app-layout>
--- modules/Auth/resources/views/index.blade.php
+++
@@ -1,5 +0,0 @@
-<x-app-layout>
-    <h1>Hello World</h1>
-
-    <p>Module: {!! config('auth.name') !!}</p>
-</x-app-layout>
