c7721ab53928e9e5f41a0a008b29a6873a57e462
Solomon Ochepa <solomonochepa@gmail.com>
2025-04-18 20:15:14 +0100

    Upgrade configs

--- config/auth.php
+++ config/auth.php
@@ -1,5 +1,7 @@
 <?php
 
+use Modules\User\App\Models\User;
+
 return [
 
     /*
@@ -62,7 +64,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', Modules\User\App\Models\User::class),
+            'model' => env('AUTH_MODEL', User::class),
         ],
 
         // 'users' => [
