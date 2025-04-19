e68f1cfdb76ff1b7e1133bc8324c473b4909fc1c
Solomon Ochepa <solomonochepa@gmail.com>
2025-04-19 05:26:54 +0100

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => redirect()->route('dashboard'))->name('home');
+// Route::view('/', 'welcome')->name('home');
+Route::redirect('/', route('dashboard'))->name('home');
 
-Route::middleware(['auth'])->group(function () {
-    Route::get('dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
 require_once 'cli.php';
