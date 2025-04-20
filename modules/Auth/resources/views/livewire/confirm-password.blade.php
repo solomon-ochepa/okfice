83f5a7cfc6711f32b928e9176a57b8c5e5f5c5a4
Solomon Ochepa <solomonochepa@gmail.com>
2025-04-20 09:09:35 +0100

    Update ConfirmPassword

---
+++ modules/Auth/resources/views/livewire/confirm-password.blade.php
@@ -0,0 +1,30 @@
+<div>
+    <x-auth::page.header :description="__('This is a secure area of the application. Please confirm your password before continuing.')" :title="__('Confirm password')" />
+    <x-themes.app.header back='dashboard' class="no-border transparent position-absolute">
+        @slot('title', '')
+        @slot('right')
+        @endslot
+    </x-themes.app.header>
+
+    <div class="section mb-5 p-2">
+        <form wire:submit="confirmPassword">
+            <div class="card">
+                <div class="card-body pb-1">
+                    <div class="form-group basic">
+                        <div class="input-wrapper">
+                            <label class="label" for="password">{{ __('Password') }}</label>
+                            <input autocomplete="new-password" autofocus class="form-control" id="password"
+                                placeholder="" required type="password" wire:model="password">
+                            <x-input.clear />
+                            <x-input.error name="password" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="form-button-group transparent">
+                <button class="btn btn-primary btn-block btn-lg" type="submit">{{ __('Confirm') }}</button>
+            </div>
+        </form>
+    </div>
+</div>
