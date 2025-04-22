fe13768a98636ec0c1bfa140565c4b647241b7b0
Solomon Ochepa <solomonochepa@gmail.com>
2025-04-22 07:47:16 +0100

    Create AppController

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
+use App\Http\Controllers\AppController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', fn() => view('welcome'))->name('home');
-Route::redirect('/', 'dashboard')->name('home');
+Route::get('/', [AppController::class, 'home'])->name('home');
 
-Route::get('dashboard', fn () => view('dashboard'))
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+// Authenticated users
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', [AppController::class, 'dashboard'])->name('dashboard');
+});
 
 // 'admin/*'
-Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
+Route::middleware(['auth', 'permission:admin.dashboard'])->prefix('admin')->name('admin.')->group(function () {
     Route::redirect('/', 'admin/dashboard');
-    Route::get('dashboard', fn () => view('livewire.admin.dashboard'))->middleware(['permission:admin.dashboard'])->name('dashboard');
+
+    Route::get('dashboard', [AppController::class, 'admin_dashboard'])->name('dashboard');
 });
 
 require_once 'cli.php';
