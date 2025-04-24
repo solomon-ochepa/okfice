c20a347e346860a7f667a0c9e118190a8d7e179b
Solomon Ochepa <solomonochepa@gmail.com>
2025-04-24 11:02:56 +0100

    Update tenancy app layout `Update sidebar layout to use Tenancy components and namespace`

--- modules/Tenancy/resources/views/components/layouts/app/sidebar.blade.php
+++ modules/Tenancy/resources/views/components/layouts/app/sidebar.blade.php
@@ -2,22 +2,24 @@
 <html class="dark" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
     <head>
-        @include('components.layouts.app.partials.head')
+        @include('tenancy::components.layouts.partials.head')
     </head>
 
     <body>
-        <x-layouts.partials.loader />
+        <x-tenancy::layouts.partials.loader />
 
         {{ $slot }}
 
         @auth
-            <x-layouts.app.sections.bottom-menu />
             <x-tenancy::layouts.app.sections.sidebar />
+            <x-tenancy::layouts.app.sections.bottom-menu />
         @endauth
-        <x-layouts.partials.add-to-home-screen />
-        <x-layouts.app.sections.cookies />
 
-        @include('components.layouts.app.partials.foot')
+        <x-tenancy::layouts.partials.add-to-home-screen />
+
+        <x-tenancy::layouts.app.sections.cookies />
+
+        @include('tenancy::components.layouts.partials.foot')
     </body>
 
 </html>
---
+++ modules/Tenancy/resources/views/components/layouts/partials/loader.blade.php
@@ -0,0 +1,3 @@
+<div id="loader">
+    <img alt="icon" class="loading-icon" src="{{ asset('assets/app/img/loading-icon.png') }}">
+</div>
