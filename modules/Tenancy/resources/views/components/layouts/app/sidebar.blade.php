d949032d042144c0c328bf0b4de72de02a4afc4d
Solomon Ochepa <solomonochepa@gmail.com>
2025-04-24 11:03:44 +0100

    Create tenancy app layout

---
+++ modules/Tenancy/resources/views/components/layouts/app/sidebar.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html class="dark" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+    <head>
+        @include('components.layouts.app.partials.head')
+    </head>
+
+    <body>
+        <x-layouts.partials.loader />
+
+        {{ $slot }}
+
+        @auth
+            <x-layouts.app.sections.bottom-menu />
+            <x-tenancy::layouts.app.sections.sidebar />
+        @endauth
+        <x-layouts.partials.add-to-home-screen />
+        <x-layouts.app.sections.cookies />
+
+        @include('components.layouts.app.partials.foot')
+    </body>
+
+</html>
