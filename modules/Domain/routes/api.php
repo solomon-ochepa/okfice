346e16f1f205c2c1026775636562b94273f3de3b
Solomon Ochepa <solomonochepa@gmail.com>
2025-04-24 14:31:13 +0100

    `Update routes to use plural form for resource paths`

--- modules/Domain/routes/api.php
+++ modules/Domain/routes/api.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use Modules\Domain\App\Http\Controllers\DomainController;
 
 Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('domain', DomainController::class)->names('domain');
+    Route::apiResource('domains', DomainController::class)->names('domain');
 });
--- modules/Domain/routes/web.php
+++ modules/Domain/routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use Modules\Domain\App\Http\Controllers\DomainController;
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('domain', DomainController::class)->names('domain');
+    Route::resource('domains', DomainController::class)->names('domain');
 });
