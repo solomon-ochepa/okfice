f5331072bbdfd76f2d93f7706d2627ed64c6a9c8
Solomon Ochepa <solomonochepa@gmail.com>
2025-04-26 11:14:40 +0100

    Refactor Account model relationships and update migration for manageable morphs

--- modules/Account/database/migrations/tenant/2024_11_16_020121_create_accounts_table.php
+++ modules/Account/database/migrations/tenant/2024_11_16_020121_create_accounts_table.php
@@ -13,17 +13,18 @@ return new class extends Migration
     {
         Schema::create('accounts', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('name')->index();
+            $table->string('name', 16)->index();
             $table->integer('number')->nullable()->unique();
             $table->decimal('amount', 18)->default(0);
             $table->char('currency', 3)->nullable()->default('NGN');
             $table->boolean('primary')->nullable();
             $table->uuidMorphs('accountable');
+            $table->nullableUuidMorphs('manageable');
             $table->timestamps();
             $table->softDeletes();
 
-            $table->unique(['name', 'number', 'currency', 'accountable_type', 'accountable_id'], 'unique_account_name');
-            $table->unique(['primary', 'number', 'currency', 'accountable_type', 'accountable_id'], 'unique_primary_account');
+            $table->unique(['name', 'number', 'currency', 'accountable_type', 'accountable_id', 'manageable_type', 'manageable_id'], 'unique_account_name');
+            $table->unique(['primary', 'number', 'currency', 'accountable_type', 'accountable_id', 'manageable_type', 'manageable_id'], 'unique_primary_account');
         });
     }
 
