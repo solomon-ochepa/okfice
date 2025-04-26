104b6f87b8e0d6b9297212e593656ff1c778e6ac
Solomon Ochepa <solomonochepa@gmail.com>
2025-04-26 11:47:22 +0100

    Add tenant OrganizationController and define API routes for organization management

---
+++ modules/Organization/app/Http/Controllers/Tenant/Api/OrganizationController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Modules\Organization\App\Http\Controllers\Tenant;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class OrganizationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json([
+            'status' => 'success',
+            'data' => [
+                // Add your data here
+            ]
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create() {}
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request) {}
+
+    /**
+     * Show the specified resource.
+     */
+    public function show($id) {}
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($id) {}
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id) {}
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id) {}
+}
--- modules/Organization/routes/tenant/web.php
+++ modules/Organization/routes/tenant/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Organization\App\Http\Controllers\OrganizationController;
+use Modules\Organization\App\Http\Controllers\Tenant\OrganizationController;
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('organizations', OrganizationController::class)->names('organization');
