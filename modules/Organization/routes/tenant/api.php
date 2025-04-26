7e4b27a1eccf80a70af1183331e18c3b21ff27e6
Solomon Ochepa <solomonochepa@gmail.com>
2025-04-26 11:58:19 +0100

    Refactor OrganizationController namespace and update route definitions for API and web to use the correct namespace

--- modules/Organization/routes/tenant/api.php
+++ modules/Organization/routes/tenant/api.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Organization\App\Http\Controllers\Tenant\OrganizationController;
+use Modules\Organization\App\Http\Controllers\Tenant\Api\OrganizationController;
 
 Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('organizations', OrganizationController::class)->names('organization');
+    Route::get('organizations', [OrganizationController::class, 'index'])->name('organization.index');
 });
