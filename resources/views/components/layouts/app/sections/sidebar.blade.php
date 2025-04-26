a0478e7d9bc69c99822f431217aa6f2b4493956c
Solomon Ochepa <solomonochepa@gmail.com>
2025-04-26 01:07:44 +0100

    Remove balance and action group sections from sidebar, update settings link, and adjust admin dashboard display

--- resources/views/components/layouts/app/sections/sidebar.blade.php
+++ resources/views/components/layouts/app/sections/sidebar.blade.php
@@ -5,39 +5,6 @@
                 <!-- profile box -->
                 <x-sidebar.profile />
 
-                <!-- balance -->
-                <div class="sidebar-balance">
-                    <div class="listview-title">Balance</div>
-                    <div class="in">
-                        {{-- <h1 class="amount">N {{ number_format(auth()->user()->balance, 2) }}</h1> --}}
-                    </div>
-                </div>
-
-                <!-- action group -->
-                <div class="action-group">
-                    @can('accounts.deposit')
-                        <a class="action-button" data-bs-target="#deposit" data-bs-toggle="modal" href="#">
-                            <div class="in">
-                                <div class="iconbox">
-                                    <ion-icon name="add-outline"></ion-icon>
-                                </div>
-                                Deposit
-                            </div>
-                        </a>
-                    @endcan
-
-                    @can('accounts.withdraw')
-                        <a class="action-button" data-bs-target="#withdraw" data-bs-toggle="modal" href="#">
-                            <div class="in">
-                                <div class="iconbox">
-                                    <ion-icon name="arrow-down-outline"></ion-icon>
-                                </div>
-                                Withdraw
-                            </div>
-                        </a>
-                    @endcan
-                </div>
-
                 <!-- menu -->
                 <div class="listview-title mt-1">Menu</div>
                 <ul class="listview flush transparent no-line image-listview">
@@ -52,57 +19,62 @@
                             </div>
                         </a>
                     </li>
-                </ul>
 
-                <!-- menu -->
-                <div class="listview-title mt-1">{{ __('Management') }}</div>
-                <ul class="listview flush transparent no-line image-listview">
-                    <li>
-                        <a class="item" href="{{ route('admin.dashboard') }}">
-                            <div class="icon-box bg-primary">
-                                <ion-icon name="home"></ion-icon>
-                            </div>
-                            <div class="in">
-                                Admin
-                                <span class="badge badge-primary"></span>
-                            </div>
-                        </a>
-                    </li>
-                </ul>
-
-                <!-- others -->
-                <div class="listview-title mt-1">
-                    <hr />
-                </div>
-                <ul class="listview flush transparent no-line image-listview">
-                    <li>
-                        <x-a route="settings">
-                            <div class="icon-box bg-primary">
-                                <ion-icon name="settings-outline"></ion-icon>
-                            </div>
-                            <div class="in">
-                                Settings
-                            </div>
-                        </x-a>
-                    </li>
-
-                    <li>
-                        <form action="{{ route('logout') }}" class="" method="POST">
-                            @csrf
-
-                            <a class="item" href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                                this.closest('form').submit();">
+                    @if (Route::has('settings'))
+                        <li>
+                            <x-a route="settings">
                                 <div class="icon-box bg-primary">
-                                    <ion-icon name="log-out-outline"></ion-icon>
+                                    <ion-icon name="settings-outline"></ion-icon>
                                 </div>
                                 <div class="in">
-                                    {{ __('Log Out') }}
+                                    Settings
+                                </div>
+                            </x-a>
+                        </li>
+                    @endif
+                </ul>
+
+                @can('admin.dashboard')
+                    <div class="listview-title mt-1">{{ __('Management') }}</div>
+                    <ul class="listview flush transparent no-line image-listview">
+                        <li>
+                            <a class="item" href="{{ route('admin.dashboard') }}">
+                                <div class="icon-box bg-primary">
+                                    <ion-icon name="home"></ion-icon>
+                                </div>
+                                <div class="in">
+                                    Admin
+                                    <span class="badge badge-primary"></span>
                                 </div>
                             </a>
-                        </form>
-                    </li>
-                </ul>
+                        </li>
+                    </ul>
+                @endcan
+
+                <!-- others -->
+                <div class="appBottomMenu d-block px-0">
+                    {{-- <div class="listview-title mt-1">
+                        <hr />
+                    </div> --}}
+                    <ul class="listview flush transparent no-line image-listview">
+                        <li>
+                            <form action="{{ route('logout') }}" class="" method="POST">
+                                @csrf
+
+                                <a class="item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                    <div class="icon-box bg-primary">
+                                        <ion-icon class="text-light" name="log-out-outline"></ion-icon>
+                                    </div>
+                                    <div class="in text-small">
+                                        {{ __('Log Out') }}
+                                    </div>
+                                </a>
+                            </form>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
