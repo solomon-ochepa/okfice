baac38394e524f3f7e26470cc6ef44c3414cbbee
Solomon Ochepa <solomonochepa@gmail.com>
2025-04-26 19:25:37 +0100

    Update admin dashboard menu items and permissions

--- modules/Tenant/resources/views/livewire/admin/dashboard.blade.php
+++ modules/Tenant/resources/views/livewire/admin/dashboard.blade.php
@@ -10,23 +10,11 @@
     </x-tenancy::layouts.app.sections.header>
 
     <div id="appCapsule">
-        <div class="listview-title mt-2">Links</div>
+        <div class="listview-title mt-2">Menu</div>
         <ul class="listview image-listview inset">
-            @can('admin.tenant.index')
-                <li>
-                    <x-a class="item" route="admin.tenant.index">
-                        <i aria-hidden="true" class="fas fa-network-wired fa-lg me-3"></i>
-                        <div class="in">
-                            <div>{{ __('Clients') }}</div>
-                            {{-- <span class="badge badge-primary">3</span> --}}
-                        </div>
-                    </x-a>
-                </li>
-            @endcan
-
             @can('admin.user.index')
                 <li>
-                    <x-a class="item" route="admin.user.index">
+                    <x-a route="admin.user.index">
                         <i aria-hidden="true" class="fa fa-users fa-lg me-3"></i>
                         <div class="in">
                             <div>{{ __('Users') }}</div>
@@ -36,9 +24,21 @@
                 </li>
             @endcan
 
-            @can('admin.transactions.index')
+            @can('admin.organization.index')
                 <li>
-                    <x-a class="item" route="admin.transaction.index">
+                    <x-a route="admin.organization.index">
+                        <i aria-hidden="true" class="fas fa-network-wired fa-lg me-3"></i>
+                        <div class="in">
+                            <div>{{ __('Organizations') }}</div>
+                            {{-- <span class="badge badge-primary">3</span> --}}
+                        </div>
+                    </x-a>
+                </li>
+            @endcan
+
+            @can('admin.transaction.index')
+                <li>
+                    <x-a route="admin.transaction.index">
                         <i aria-hidden="true" class="fa fa-users fa-lg me-3"></i>
                         <div class="in">
                             <div>{{ __('Transactions') }}</div>
@@ -47,7 +47,6 @@
                     </x-a>
                 </li>
             @endcan
-
         </ul>
     </div>
 </x-tenancy::layouts.app>
