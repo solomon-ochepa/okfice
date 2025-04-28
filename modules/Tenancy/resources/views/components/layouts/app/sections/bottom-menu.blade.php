947eee5829e355f6cf5fcb57a2ff4d9ac83f76cc
Solomon Ochepa <solomonochepa@gmail.com>
2025-04-28 03:37:58 +0100

    Add admin dashboard link to bottom menu with authorization check

--- modules/Tenancy/resources/views/components/layouts/app/sections/bottom-menu.blade.php
+++ modules/Tenancy/resources/views/components/layouts/app/sections/bottom-menu.blade.php
@@ -12,4 +12,13 @@
             <strong>Settings</strong>
         </div>
     </x-a>
+
+    @can('dashboard', '')
+        <x-a can="admin.dashboard" route="admin.dashboard">
+            <div class="col">
+                <ion-icon name="settings-outline"></ion-icon>
+                <strong>Admin</strong>
+            </div>
+        </x-a>
+    @endcan
 </div>
