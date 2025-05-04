0454066fd30216ccb4568423f813781bc250213b
Solomon Ochepa <solomonochepa@gmail.com>
2025-05-04 08:50:42 +0100

    Refactor AppServiceProvider: organize imports and enhance feature availability handling

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,13 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Request;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Pennant\Feature;
+use Laravel\Pennant\Middleware\EnsureFeaturesAreActive;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,13 +27,19 @@ class AppServiceProvider extends ServiceProvider
     {
         Feature::discover();
 
+        EnsureFeaturesAreActive::whenInactive(
+            function (Request $request, array $features) {
+                abort(403, 'Feature Unavailable! Please contact your system admin for further support.');
+            }
+        );
+
         /**
          * Implicitly grant roles all permissions
          *
          * This works in the app by using gate-related functions like auth()->user->can() and @can()
          * NOTE: Gate::after rules need to return null rather than false, else it will interfere with normal policy operation.
          */
-        Gate::after(fn ($user, $ability) => $user->hasRole(['super-admin', 'admin', 'developer']) ? true : null);
+        Gate::after(fn($user, $ability) => $user->hasRole(['super-admin', 'admin', 'developer']) ? true : null);
 
         Paginator::useBootstrap();
 
