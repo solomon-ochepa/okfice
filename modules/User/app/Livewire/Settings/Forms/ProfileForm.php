15a4ab57c31f0fc6a8100ef618632da693ee09d5
Solomon Ochepa <solomonochepa@gmail.com>
2025-05-19 11:31:19 +0100

    Implement profile form with validation and update functionality; refactor profile view to use new form structure

---
+++ modules/User/app/Livewire/Settings/Forms/ProfileForm.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Modules\User\app\Livewire\Settings\Forms;
+
+use Illuminate\Support\Facades\Auth;
+use Livewire\Form;
+use Modules\User\App\Models\User;
+use Illuminate\Validation\Rule;
+
+class ProfileForm extends Form
+{
+    public ?string $first_name = '';
+
+    public ?string $last_name = '';
+
+    public ?string $username = '';
+
+    public ?string $email = '';
+
+    public ?string $phone = '';
+
+    public function rules()
+    {
+        return [
+            'first_name' => ['required', 'string', 'max:32'],
+            'last_name' => ['required', 'string', 'max:32'],
+            'username' => ['required', 'string', 'max:16', 'alpha_dash', Rule::unique(User::class)->ignore(Auth::id())],
+            'phone' => ['nullable', 'string', 'max:16', Rule::unique(User::class)->ignore(Auth::id())],
+            'email' => [
+                'required',
+                'string',
+                'lowercase',
+                'email',
+                'max:255',
+                Rule::unique(User::class)->ignore(Auth::id()),
+            ],
+        ];
+    }
+
+    public function update()
+    {
+        $validated = $this->validate();
+        $user = Auth::user();
+
+        $user->fill($validated);
+
+        // If the email address is changed, we need to set the email_verified_at field to null
+        if ($user->isDirty('email')) {
+            $user->email_verified_at = null;
+        }
+
+        $user->save();
+    }
+}
---
+++ resources/views/components/input.blade.php
@@ -0,0 +1,16 @@
+@props(['label' => null])
+<div class="form-group basic">
+    <div class="input-wrapper">
+        @if ($label)
+            <label class="label" for="">{{ $label }}</label>
+        @endif
+        <input {{ $attributes->merge(['class' => 'form-control', 'type' => 'text']) }} id="amount1"
+            placeholder="Enter an Amount" type="text" />
+        <i class="clear-input">
+            <ion-icon name="close-circle"></ion-icon>
+        </i>
+        @error($attributes->get('wire:model') ?? $attributes->get('name'))
+            <span class="text-danger">{{ $message }}</span>
+        @enderror
+    </div>
+</div>
