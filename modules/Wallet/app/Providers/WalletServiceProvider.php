0628f7200b087c1494d096a99ba8008f049bdd94
Solomon Ochepa <solomonochepa@gmail.com>
2025-05-24 11:25:11 +0100

    Comment out RouteServiceProvider registration in WalletServiceProvider

--- modules/Wallet/app/Providers/WalletServiceProvider.php
+++ modules/Wallet/app/Providers/WalletServiceProvider.php
@@ -35,7 +35,7 @@ class WalletServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->register(EventServiceProvider::class);
-        $this->app->register(RouteServiceProvider::class);
+        // $this->app->register(RouteServiceProvider::class);
     }
 
     /**
